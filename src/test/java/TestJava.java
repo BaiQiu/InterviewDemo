37fa8f40d2d669ab0dfed58c6b6eb0871605c1e4
BaiQiu <1739585821@qq.com>
2017-12-19 08:39:35 +0800

    测试提交

--- src/test/java/TestJava.java
+++ src/test/java/TestJava.java
@@ -2,15 +2,14 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 
 /**
  * @author rxliuli
  */
 public class TestJava {
-  public static void main(String[] args) throws SQLException {
+  public static void main(String[] args) {
     ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/spring-dao.xml");
     DataSource dataSource = (DataSource) context.getBean("dataSource");
-    System.out.println(dataSource);
+    System.out.println("数据源: " + dataSource);
   }
 }
