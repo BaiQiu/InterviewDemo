c3c4f740e889d1ff8473b566d149a7f57f6b86b8
BaiQiu <1739585821@qq.com>
2017-12-19 10:13:33 +0800

    修复数据库远程连接

--- src/test/java/TestJava.java
+++ src/test/java/TestJava.java
@@ -1,3 +1,6 @@
+import com.interview.dao.TopicMapper;
+import com.interview.entity.Topic;
+import com.interview.util.SnowflakeIdUtil;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,5 +14,8 @@ public class TestJava {
     ApplicationContext context = new ClassPathXmlApplicationContext("classpath:spring/spring-dao.xml");
     DataSource dataSource = (DataSource) context.getBean("dataSource");
     System.out.println("数据源: " + dataSource);
+    long typeId = SnowflakeIdUtil.getId();
+    Topic topic = new Topic(SnowflakeIdUtil.getId(), typeId, "问题的标题", "参考答案");
+    System.out.println(context.getBean(TopicMapper.class).insert(topic));
   }
 }
