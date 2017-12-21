497516beb3a3d4afd8f3fc9e9e2429ecfb5c382a
BaiQiu <1739585821@qq.com>
2017-12-21 10:55:19 +0800

    黎棹禾 完成查询所有学生信息静态页面

--- src/main/webapp/statics/background/js/userList.js
+++ src/main/webapp/statics/background/js/userList.js
@@ -10,3 +10,29 @@ $(document).ready(function () {
   });
 });
 
+$(function () {
+  $("#pagination3").pagination({
+    currentPage: 1,
+    totalPage: 16,
+    isShow: true,
+    count: 7,
+    homePageText: "首页",
+    endPageText: "尾页",
+    prevPageText: "上一页",
+    nextPageText: "下一页",
+    callback: function(current) {
+      $("#current3").text(current)
+    }
+  });
+
+  $("#getPage").on("click", function() {
+    var info = $("#pagination3").pagination("getPage");
+    alert("当前页数：" + info.current + ",总页数：" + info.total);
+  });
+
+  $("#setPage").on("click", function() {
+    $("#pagination3").pagination("setPage", 1, 10);
+  });
+});
+
+
