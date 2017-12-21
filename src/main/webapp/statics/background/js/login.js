dcc1c66c569a5cbaa5159a678672c9a52ed4d832
rxliuli <rxliuli@gmail.com>
2017-12-21 10:51:22 +0800

    王凯 更新登陆的静态页面，样式和脚本文件

--- src/main/webapp/statics/background/js/login.js
+++ src/main/webapp/statics/background/js/login.js
@@ -2,21 +2,32 @@ $(function () {
   /**
    * 管理员登陆.
    */
-  $("button:submit").click(function () {
-    var username = $("#username").val();
+  $(".form-login button:submit").click(function () {
+    //获得登陆名和密码
+    var adminName = $("#adminName").val();
     var password = $("#password").val();
+
+    //非空校验
+    if (adminName.trim() === ''
+      || password.trim() === '') {
+      alert("用户名或密码为空!");
+      return false;
+    }
+
+    //进行 ajax 验证
     $.ajax({
       url: "/admin/loginExecute",
       type: "POST",
       dataType: "json",
       data: {
-        username: username,
+        adminName: adminName,
         password: password
       },
       success: function (json) {
         var success = json.success;
         if (success) {
           open("/admin/index", "_self");
+
         } else {
           alert(json.message);
         }
@@ -25,6 +36,7 @@ $(function () {
         alert("网络错误啦！")
       }
     });
+    //无论是否成功都不会提交表单(然而如果真的成功就会直接跳转到其他页面了 233333)
     return false;
   })
 });
