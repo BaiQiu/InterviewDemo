f5419aaa2c45a42224d75b18f5743059cc002c34
rxliuli <rxliuli@gmail.com>
2017-12-22 15:15:53 +0800

    王凯 考试成绩 dao 层添加根据用户编号和复数考试 id 去查询成绩的方法并实现

--- src/main/java/com/interview/dao/ResultMapper.java
+++ src/main/java/com/interview/dao/ResultMapper.java
@@ -4,6 +4,7 @@ import com.interview.entity.Result;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 学生成绩数据访问接口.
@@ -18,6 +19,14 @@ public interface ResultMapper {
    * @return 成绩列表
    */
   List<Result> listByUserId(@Param("userId") Long userId);
+
+  /**
+   * 根据用户编号和复数考试的 id 查询成绩.
+   *
+   * @param map 参数映射表
+   * @return 查询到的成绩列表
+   */
+  List<Result> listByUserIdAndExaminations(Map<String, Object> map);
 
   /**
    * 添加一个学生的成绩信息.
--- src/test/java/com/interview/dao/ResultMapperTest.java
+++ src/test/java/com/interview/dao/ResultMapperTest.java
@@ -1,12 +1,16 @@
 package com.interview.dao;
 
 import com.github.pagehelper.PageHelper;
+import com.interview.entity.Examination;
 import com.interview.entity.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 import util.BaseTestNGUtil;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,6 +20,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ResultMapperTest extends BaseTestNGUtil {
   @Autowired
   private ResultMapper resultMapper;
+  @Autowired
+  private ExaminationMapper examinationMapper;
 
   @Test
   public void testListByUserId() throws Exception {
@@ -23,5 +29,29 @@ public class ResultMapperTest extends BaseTestNGUtil {
     PageHelper.startPage(1, resultNumber);
     List<Result> resultList = resultMapper.listByUserId(393730760231751680L);
     assertThat(resultList).hasSize(resultNumber);
+  }
+
+  @Test
+  public void testListByUserIdAndExaminations() throws Exception {
+    //获取最近 20 次的考试
+    Long userId = 393730760231751680L;
+    List<Examination> examinationList = examinationMapper.listByLimit(20);
+    List<Long> examinationIdList = examinationList.stream()
+      .map(Examination::getId)
+      .collect(Collectors.toList());
+    Integer resultNumber = 20;
+
+    //参数
+    Map<String, Object> map = new HashMap<>();
+    map.put("userId", userId);
+    map.put("examinationIdList", examinationIdList);
+    map.put("resultNumber", resultNumber);
+
+    //查询
+    List<Result> resultList = resultMapper.listByUserIdAndExaminations(map);
+    assertThat(resultList)
+      .hasSize(resultNumber)
+      .allMatch(result -> result.getUserLoginId().equals(userId))
+      .allMatch(result -> examinationIdList.contains(result.getExaminationId()));
   }
 }
