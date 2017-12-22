97eb04ff3e7f3ab0008d9148b14e88c7262acd77
rxliuli <rxliuli@gmail.com>
2017-12-22 09:44:50 +0800

    王凯 添加一个默认的日期时期类型的格式字符串

--- src/main/java/com/interview/util/ConstantsUtil.java
+++ src/main/java/com/interview/util/ConstantsUtil.java
@@ -7,14 +7,31 @@ package com.interview.util;
  */
 public final class ConstantsUtil {
   /**
-   * 保存用户的session字段
+   * 保存用户 session 的字段名.
    */
   public static final String USER_SESSION = "userSession";
 
+  /**
+   * 保存管理员 session 的字段名.
+   */
   public static final String ADMIN_SESSION = "ADMIN_SESSION";
+
+  /**
+   * 日期格式化模式字符串.
+   * 因为 html 的默认日期格式是 yyyy-MM-ddThh:mm
+   */
+  public static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd hh:mm:ss";
+  /**
+   * 添加考试时题目数量的最小值.
+   */
+  public static final int TOPIC_MIN_NUMBER = 10;
+  /**
+   * 添加考试时题目数量的最大值.
+   */
+
+  public static final int TOPIC_MAX_NUMBER = 60;
 
   private ConstantsUtil() {
   }
-
 
 }
