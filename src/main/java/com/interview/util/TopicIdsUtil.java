1050e1a59d4ed784e8f45e83595610955e99ec74
rxliuli <rxliuli@gmail.com>
2017-12-22 09:43:32 +0800

    王凯 关于面试题目的工具类（主要是为了解决考试中复数 id 的连接和拆分问题）

--- src/main/java/com/interview/util/TopicIdsUtil.java
+++ src/main/java/com/interview/util/TopicIdsUtil.java
@@ -2,6 +2,9 @@ package com.interview.util;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 考试题目的工具类.
  * 主要用于拆分在一场考试中包含有所有考试题目编号的字符串，以及反向的操作
@@ -24,11 +27,21 @@ public final class TopicIdsUtil {
   /**
    * 连接多个考试的编号成字符串.
    *
-   * @param topicIds 多个考试编号
+   * @param topicIds 多个考试编号/或者数组
    * @return 连接完成的字符串
    */
   public static String join(Long... topicIds) {
     return StringUtils.join(topicIds, LINE);
+  }
+
+  /**
+   * 连接多个考试的编号成字符串.
+   *
+   * @param topicList 多个考试编号组成的列表
+   * @return 连接完成的字符串
+   */
+  public static String join(List<Long> topicList) {
+    return join(topicList.toArray(new Long[]{}));
   }
 
   /**
@@ -44,5 +57,15 @@ public final class TopicIdsUtil {
       longs[i] = Long.valueOf(split[i]);
     }
     return longs;
+  }
+
+  /**
+   * 将连接多个考试的编号成的字符串拆分成编号列表.
+   *
+   * @param topicIds 多个考试的编号连接成的字符串
+   * @return 考试编号列表
+   */
+  public static List<Long> splitToList(String topicIds) {
+    return Arrays.asList(splitToArray(topicIds));
   }
 }
