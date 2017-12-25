919c2a79c2d8bcd369b10ca6a82a1ae0827e2602
BaiQiu <1739585821@qq.com>
2017-12-25 09:45:25 +0800

    Merge branch 'master' of D:\Text\SSM\InterviewDemo\InterviewDemo with conflicts.

--- src/test/java/com/interview/dao/TopicMapperTest.java
+++ src/test/java/com/interview/dao/TopicMapperTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.annotations.Test;
 import util.BaseTestNGUtil;
 
-import java.sql.Timestamp;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,7 +20,6 @@ public class TopicMapperTest extends BaseTestNGUtil {
   @Test
   public void testInsert() throws Exception {
     long typeId = SnowflakeIdUtil.getId();
-    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
     Topic topic = new Topic(SnowflakeIdUtil.getId(), typeId, "问题的标题", "参考答案", null);
     int i = topicMapper.insert(topic);
     assertThat(i).isEqualTo(1);
